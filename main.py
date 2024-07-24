47685ffed7eebfcbe6e240cb99300098ac78b68b
Ykzl <1969497633@qq.com>
2024-07-25 04:46:18 +0800

    增加场地编辑功能；增加用于存读档的按钮（键盘存读快捷键保留）

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from pynput import keyboard, mouse
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtCore import Qt, QTimer, QDateTime, QPoint
-from PyQt5.QtGui import QPainter, QColor, QFont, QPen
+from PyQt5.QtGui import QPainter, QColor, QFont, QPen, QCursor, QMouseEvent
 import pygetwindow as gw
 import win32gui
 import ctypes
@@ -17,9 +17,13 @@ def getActiveWindowTitle():
 
 def getWindow(title="Bejeweled 3"):
     try:
-        window = gw.getWindowsWithTitle(title)[0]
-        x, y, width, height = window.left + 8, window.top + 32, window.width - 16, window.height - 40
-        return (x + width, y, int(16 / 9 * height - width), height)
+        for window in gw.getWindowsWithTitle(title):
+            if window.title != title:
+                continue
+            x, y, width, height = window.left + 8, window.top + 32, window.width - 16, window.height - 40
+            return (x + width, y, int(16 / 9 * height - width), height)
+        print(f"Window with title '{title}' not found.")
+        return (0, 0, 533, 1200)
     except IndexError:
         print(f"Window with title '{title}' not found.")
         return (0, 0, 533, 1200)
@@ -77,25 +81,74 @@ def write4Bytes(address, data, offsets=[], dataType="int"):
 pid = getPID("Bejeweled3.exe")
 processHandle = ctypes.windll.kernel32.OpenProcess(0x1F0FFF, False, pid)
 color = {
-    0: QColor(255, 0, 0),
-    1: QColor(255, 255, 255),
-    2: QColor(0, 255, 0),
-    3: QColor(255, 255, 0),
-    4: QColor(128, 0, 255),
-    5: QColor(255, 128, 0),
-    6: QColor(0, 0, 255),
+    -1: {"color": QColor(0, 0, 0, 255), "name": ""},
+    0: {"color": QColor(255, 0, 0, 255), "name": "红"},
+    1: {"color": QColor(255, 255, 255, 255), "name": "白"},
+    2: {"color": QColor(0, 255, 0, 255), "name": "绿"},
+    3: {"color": QColor(255, 255, 0, 255), "name": "黄"},
+    4: {"color": QColor(128, 0, 255, 255), "name": "紫"},
+    5: {"color": QColor(255, 128, 0, 255), "name": "橙"},
+    6: {"color": QColor(0, 0, 255, 255), "name": "蓝"},
+    4294967295: {"color": QColor(0, 0, 0, 255), "name": ""},
 }
 saveStates = [None for i in range(12)]
 keyPressed = []
 logs = ""
 logsTime = 0
+cursor = None
+
+
+def saveOrLoad(i):
+    global logs, logsTime
+    if "shift" in keyPressed:
+        saveStates[i] = [[{"color": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220]), "special": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228])} for ix in range(8)] for iy in range(8)]
+        logs, logsTime = f"已保存至存档 {i}", time.time()
+    else:
+        if not saveStates[i]:
+            logs, logsTime = f"存档 {i} 不存在", time.time()
+            return
+        for ix in range(8):
+            for iy in range(8):
+                write4Bytes(0x008E1730, saveStates[i][iy][ix]["color"], [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220])
+                write4Bytes(0x008E1730, saveStates[i][iy][ix]["special"], [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228])
+        logs, logsTime = f"已读取存档 {i}", time.time()
 
 
 class Window(QWidget):
+
     def __init__(self, parent=None):
+
+        def createButtonSavestate(i):
+            button = QPushButton(f"{i}", self)
+            button.setGeometry(15 + 50 * i, self.height() - 110, 50, 50)
+            button.clicked.connect(lambda: saveOrLoad(i))
+            return button
+
+        def createButtonCursorColor(i):
+            button = QPushButton(f"{color[i]['name']}", self)
+            button.setGeometry(15 + 50 * i, 975, 50, 50)
+            button.setEnabled(False)
+            button.clicked.connect(lambda: write4Bytes(0x008E1730, i, [0xBE8, 0xF8 + 4 * cursor[0] + 32 * cursor[1], 0x220]))
+            return button
+
+        def createButtonCursorSpecial(i, xpos, name):
+            button = QPushButton(name, self)
+            button.setGeometry(15 + 50 * xpos, 1025, 50, 50)
+            button.setEnabled(False)
+            button.clicked.connect(lambda: write4Bytes(0x008E1730, i, [0xBE8, 0xF8 + 4 * cursor[0] + 32 * cursor[1], 0x228]))
+            return button
+
         super().__init__(parent)
         self.setWindowTitle("Bejeweled 3 Helper")
         self.setGeometry(*getWindow())
+        self.buttonSavestate = [createButtonSavestate(i) for i in range(10)]
+        self.buttonCursorColor = [createButtonCursorColor(i) for i in range(7)]
+        self.buttonCursorSpecial = []
+        self.buttonCursorSpecial.append(createButtonCursorSpecial(0, 0, "无"))
+        self.buttonCursorSpecial.append(createButtonCursorSpecial(1, 1, "火"))
+        self.buttonCursorSpecial.append(createButtonCursorSpecial(2, 2, "超"))
+        self.buttonCursorSpecial.append(createButtonCursorSpecial(4, 3, "闪"))
+        self.buttonCursorSpecial.append(createButtonCursorSpecial(5, 4, "星"))
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateData)
@@ -103,6 +156,21 @@ class Window(QWidget):
 
     def updateData(self):
         self.update()
+
+    def mousePressEvent(self, event: QMouseEvent):
+        global cursor
+        if event.button() == Qt.LeftButton:
+            x, y = event.pos().x(), event.pos().y()
+            if x >= 10 and x < 522 and y >= 400 and y < 912:
+                ix, iy = int((x - 10) / 64), int((y - 400) / 64)
+                if cursor != (ix, iy):
+                    cursor = (ix, iy)
+                    for button in self.buttonCursorColor + self.buttonCursorSpecial:
+                        button.setEnabled(True)
+                    return
+            cursor = None
+            for button in self.buttonCursorColor + self.buttonCursorSpecial:
+                button.setEnabled(False)
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -124,7 +192,21 @@ class Window(QWidget):
             "gem": [read4Bytes(0x008E170C, [0xB80, 0xD0 + i * 4]) for i in range(11)],
         }
         mode = ""
-        field = [[{"color": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220]), "special": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228])} for ix in range(8)] for iy in range(8)]
+        field = [
+            [
+                {
+                    "address": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy]),
+                    "color": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220]),
+                    "special": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228]),
+                }
+                for ix in range(8)
+            ]
+            for iy in range(8)
+        ]
+        score = read4Bytes(0x008E1730, [0xBE8, 0xD24])
+        progressAnim = read4Bytes(0x008E1730, [0xBE8, 0xD68], "float")
+        progress = read4Bytes(0x008E1730, [0xBE8, 0xE00])
+        level = read4Bytes(0x008E1730, [0xBE8, 0xE04])
         if mode == "Ice Storm":
             iceCombo = read4Bytes(0x008E1730, [0xBE8, 0x36C8])
             currentTime = read4Bytes(0x008E1730, (0xBE8, 0xE38))
@@ -134,32 +216,48 @@ class Window(QWidget):
 
         # 绘制
         drawX, drawY = 10, 10
-        sideLength = 64
         self.drawText(painter, drawX, drawY, systemTime)
-        drawY += 25
+        drawY += 30
         self.drawText(painter, drawX, drawY, f"Process ID: {pid}")
-        drawY += 25
+        drawY += 30
         self.drawText(painter, drawX, drawY, f"获取信息耗时: {endReadTime-startReadTime:.3f}")
-        drawY += 25
+        drawY += 30
         self.drawText(painter, drawX, drawY, f"总宝石数: {statistics['gem'][0]}")
-        drawY += 40
+        drawY += 30
+        self.drawText(painter, drawX, drawY, f"等级: {level} 分数: {score}")
+        drawY += 30
+        self.drawText(painter, drawX, drawY, f"进展: {progressAnim*100:.2f}% ({progress}/{level * 750 + 2500 if level<30 else 25000})")
+        drawY = 350
+        mousePos = self.mapFromGlobal(QCursor.pos())
+        for i, button in enumerate(self.buttonSavestate):
+            if button.geometry().contains(mousePos) and saveStates[i]:
+                field = saveStates[i]
+                self.drawText(painter, drawX, drawY, f"*预览: 存档 {i}")
+        drawY += 50
         for iy in range(8):
             for ix in range(8):
-                gridX, gridY = drawX + ix * sideLength, drawY + iy * sideLength
-                gridColor = color.get(field[iy][ix]["color"], QColor(0, 0, 0))
-                self.drawRect(painter, gridX, gridY, gridX + sideLength, gridY + sideLength, gridColor)
+                gridX, gridY = drawX + ix * 64, drawY + iy * 64
+                gridColor = color[field[iy][ix]["color"]]["color"]
+                self.drawRect(painter, gridX, gridY, gridX + 64, gridY + 64, gridColor)
                 match (field[iy][ix]["special"]):
                     case 1:
-                        self.drawText(painter, gridX + 18, gridY + 18, "火")
+                        self.drawText(painter, gridX + 19, gridY + 19, "火")
                     case 2:
-                        self.drawText(painter, gridX + 18, gridY + 18, "超", Qt.white)
+                        self.drawText(painter, gridX + 19, gridY + 19, "超", Qt.white)
                     case 4:
-                        self.drawText(painter, gridX + 18, gridY + 18, "闪")
+                        self.drawText(painter, gridX + 19, gridY + 19, "闪")
                     case 5:
-                        self.drawText(painter, gridX + 18, gridY + 18, "星")
+                        self.drawText(painter, gridX + 19, gridY + 19, "星")
                     case _:
                         pass
-        drawY += 300
+        if cursor:
+            cursorAddress = field[cursor[1]][cursor[0]]["address"]
+            cursorColor = field[cursor[1]][cursor[0]]["color"]
+            cursorSpecial = field[cursor[1]][cursor[0]]["special"]
+            cursorPen = QPen(QColor(0, 0, 0, 255), 2) if cursorColor <= 6 else QPen(QColor(255, 255, 255, 255), 2)
+            self.drawRect(painter, drawX + 64 * cursor[0] + 8, drawY + 64 * cursor[1] + 8, drawX + 64 * cursor[0] + 56, drawY + 64 * cursor[1] + 56, brush=Qt.NoBrush, border=cursorPen)
+            self.drawText(painter, drawX, drawY + 520, f"地址: 0x{hex(cursorAddress)[2:].upper()}")
+            self.drawText(painter, drawX, drawY + 550, f"颜色: {color[cursorColor]['name']}")
 
         if mode == "Ice Storm":
             ratio = (iceComboAllowTime + iceComboTime - currentTime) / iceComboAllowTime
@@ -182,7 +280,7 @@ class Window(QWidget):
         textRect.moveTopLeft(QPoint(X, Y))
         painter.drawText(textRect, Qt.AlignLeft, text)
 
-    def drawRect(self, painter, X1, Y1, X2, Y2, brush=QColor(255, 255, 255), border=QPen(QColor(0, 0, 0), 1)):
+    def drawRect(self, painter, X1, Y1, X2, Y2, brush=QColor(255, 255, 255, 255), border=QPen(QColor(0, 0, 0, 255), 1)):
         if X2 < X1 or Y2 < Y1:
             return
         painter.setPen(border)
@@ -208,18 +306,7 @@ def onKeyboardPress(key):
             if title != "Bejeweled 3" and title != "Bejeweled 3 Helper":
                 return
             keyID = key.vk - 48
-            if "shift" in keyPressed:
-                saveStates[keyID] = [[{"color": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220]), "special": read4Bytes(0x008E1730, [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228])} for ix in range(8)] for iy in range(8)]
-                logs, logsTime = f"已保存至存档 {keyID}", time.time()
-            else:
-                if not saveStates[keyID]:
-                    logs, logsTime = f"存档 {keyID} 不存在", time.time()
-                    return
-                for ix in range(8):
-                    for iy in range(8):
-                        write4Bytes(0x008E1730, saveStates[keyID][iy][ix]["color"], [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x220])
-                        write4Bytes(0x008E1730, saveStates[keyID][iy][ix]["special"], [0xBE8, 0xF8 + 4 * ix + 32 * iy, 0x228])
-                logs, logsTime = f"已读取存档 {keyID}", time.time()
+            saveOrLoad(keyID)
     except AttributeError:
         if key.name not in keyPressed:
             keyPressed.append(key.name)
